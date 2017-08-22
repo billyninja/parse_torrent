6ff9f013a2e076eabd8b3eff326ab87ce7f67289
billyninja <joaoeduardocm@gmail.com>
2017-08-21 22:00:40 -0300

    fmt

--- main.go
+++ main.go
@@ -1,79 +1,80 @@
 package main
 
 import (
-    "bytes"
-    "io/ioutil"
-    "strconv"
-    "fmt"
+	"bytes"
+	"fmt"
+	"io/ioutil"
+	"strconv"
 )
 
 var info []string
+var pieces []string
 
 func proc() {
-    fullchunk, err := ioutil.ReadFile("sample.torrent")
-    maxl := len(fullchunk)
-    if err != nil {
-        return
-    }
-    for i, byt := range fullchunk {
+	fullchunk, err := ioutil.ReadFile("sample.torrent")
+	maxl := len(fullchunk)
+	if err != nil {
+		return
+	}
+	for i, byt := range fullchunk {
 
-        // 105 -> i (integer value)
-        if byt == 105 {
-            nxt := ""
-            for j := i + 1;; j++ {
-                seg := fullchunk[j:j+1]
-                if bytes.ContainsAny(seg, "1234567890") {
-                    fmt.Printf("i>%s\n", seg)
-                    nxt += string(seg)
-                } else {
-                    fmt.Printf("i breaking at %q\n", seg)
-                    break
-                }
-            }
-            info = append(info, nxt)
-        }
+		// 105 -> i (integer value)
+		if byt == 105 {
+			nxt := ""
+			for j := i + 1; ; j++ {
+				seg := fullchunk[j : j+1]
+				if bytes.ContainsAny(seg, "1234567890") {
+					fmt.Printf("i>%s\n", seg)
+					nxt += string(seg)
+				} else {
+					fmt.Printf("i breaking at %q\n", seg)
+					break
+				}
+			}
+			info = append(info, nxt)
+		}
 
-        // 58 -> :
-        if byt == 58 {
-            prev := ""
-            for j := 1; j <= 4; j++ {
-                seg := fullchunk[(i-j):((i-j)+1)]
-                fmt.Printf(">%+q\n", seg)
-                if bytes.ContainsAny(seg, "1234567890") {
-                    println("curr", prev)
-                    prev = string(seg) + prev
-                } else {
-                    fmt.Printf("breaking at>%+q\n", seg)
-                    break
-                }
-            }
-            if prev != "" {
-                stride, err := strconv.Atoi(prev)
-                if err != nil {
-                    fmt.Printf("%v", err)
-                    return
-                }
-                println("final s:", prev)
-                println("final i:", stride)
-                if (i+1)+stride > maxl {
-                    return
-                }
+		// 58 -> :
+		if byt == 58 {
+			prev := ""
+			for j := 1; j <= 4; j++ {
+				seg := fullchunk[(i - j):((i - j) + 1)]
+				fmt.Printf(">%+q\n", seg)
+				if bytes.ContainsAny(seg, "1234567890") {
+					println("curr", prev)
+					prev = string(seg) + prev
+				} else {
+					fmt.Printf("breaking at>%+q\n", seg)
+					break
+				}
+			}
+			if prev != "" {
+				stride, err := strconv.Atoi(prev)
+				if err != nil {
+					fmt.Printf("%v", err)
+					return
+				}
+				println("final s:", prev)
+				println("final i:", stride)
+				if (i+1)+stride > maxl {
+					return
+				}
 
-                sc := fullchunk[i+1:(i+1)+stride]
-                info = append(info, string(sc))
-                // LEAP
-                i = (i+1) + stride
-                continue
-            }
-        }
+				sc := fullchunk[i+1 : (i+1)+stride]
+				info = append(info, string(sc))
+				// LEAP
+				i = (i + 1) + stride
+				continue
+			}
+		}
 
-    }
-    //fmt.Printf("%s", fullchunk)
+	}
+	//fmt.Printf("%s", fullchunk)
 }
 
 func main() {
-    proc()
-    for _, p := range info {
-        println(p)
-    }
+	proc()
+	for _, p := range info {
+		println(p)
+	}
 }
